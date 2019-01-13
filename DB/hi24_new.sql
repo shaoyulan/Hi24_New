27c29ccbb2900af97af10b722fce3d186d81c453
shaoyu <poterpoter2@gmail.com>
2019-01-14 06:42:48 +0800

    update DB backup file

--- DB/hi24_new.sql
+++ DB/hi24_new.sql
@@ -2,10 +2,10 @@
 -- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- 主機: localhost:8889
--- 產生時間： 2018-12-18 08:37:12
--- 伺服器版本: 5.6.34-log
--- PHP 版本： 7.2.1
+-- Host: localhost:8889
+-- Generation Time: Jan 13, 2019 at 10:40 PM
+-- Server version: 5.6.34-log
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,36 +19,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `hi24`
+-- Database: `hi24_new`
 --
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `header_ad`
+-- Table structure for table `header_ad`
 --
 
 CREATE TABLE `header_ad` (
   `id` int(11) NOT NULL,
-  `ad_stat` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `img_url` varchar(90) COLLATE utf8_unicode_ci NOT NULL,
+  `transition` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `speed` int(11) NOT NULL,
+  `page_url` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `img_url` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `alt` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `caption_stat` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `caption_text` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
+  `dataX` int(11) NOT NULL,
+  `dataY` int(11) NOT NULL,
+  `title` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `caption` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 資料表的匯出資料 `header_ad`
+-- Dumping data for table `header_ad`
 --
 
-INSERT INTO `header_ad` (`id`, `ad_stat`, `img_url`, `caption_stat`, `caption_text`) VALUES
-(1, 'item active', 'images/in_img/b01.jpg', 'carousel-caption', 'Caption 1'),
-(2, 'item', 'images/in_img/b02.jpg', NULL, NULL),
-(3, 'item', 'http://placehold.it/1900x1080&text=Slide Three', 'carousel-caption', 'Caption 3');
+INSERT INTO `header_ad` (`id`, `transition`, `speed`, `page_url`, `img_url`, `alt`, `caption_stat`, `dataX`, `dataY`, `title`, `caption`) VALUES
+(1, 'fade', 1500, 'product/product.html', 'images/in_img/b01.jpg', 'slidebg1', 'caption dark sfb fadeout', 145, 170, 'New Hi24 Grand Opening', 'UP TO 35% OFF WOMEN’S SWEATER'),
+(2, 'zoomout', 1000, 'product/product.html', 'images/in_img/b02.jpg', 'darkblurbg', 'caption dark sfb fadeout', 550, 170, 'END OF SEASON SALE', 'UP TO 35% OFF WOMEN’S SWEATER'),
+(3, 'fadetotopfadefrombottom', 1000, 'product/product.html', 'images/in_img/b03.jpg', 'darkblurbg', 'caption sfb fadeout', 750, 60, '', '');
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `member`
+-- Table structure for table `member`
 --
 
 CREATE TABLE `member` (
@@ -58,7 +64,7 @@ CREATE TABLE `member` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 資料表的匯出資料 `member`
+-- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`id`, `name`, `password`) VALUES
@@ -67,20 +73,44 @@ INSERT INTO `member` (`id`, `name`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `product_default_photos`
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
+  `img_url` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `title` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  `context` varchar(300) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `news`
+--
+
+INSERT INTO `news` (`id`, `img_url`, `title`, `date`, `context`) VALUES
+(0, 'images/from_the_blog_01.jpg', '宅配運費調整公告', '2018-12-17', '親愛的會員您好：<br>\r\n					因應宅配運費調整，2019年1月1日起，宅配相關訂單運費，將取消免運優惠，運費統一為60元。<br>\r\n					超商取貨訂單，相關免運及運費計算不變，並請依最新行銷活動為準。<br>'),
+(1, 'images/from_the_blog_02.jpg', '重要通知！全面採用電子發票', '2018-10-22', '親愛的會員您好：<br>\r\n				感謝您長期的支持與肯定，為了配合財政部推廣發票電子化及加速您的退款作業程序，自2018/10/19起將全面採用電子發票，您可於出貨24小時後至《 訂單查詢 》中查閱訂單發票內容：'),
+(2, 'images/from_the_blog_03.jpg', '宅配運費調整公告', '2018-12-17', '親愛的會員您好：<br>\r\n					因應宅配運費調整，2019年1月1日起，宅配相關訂單運費，將取消免運優惠，運費統一為60元。<br>\r\n					超商取貨訂單，相關免運及運費計算不變，並請依最新行銷活動為準。<br>'),
+(3, 'images/from_the_blog_04.jpg', '重要通知！全面採用電子發票', '2018-10-22', '親愛的會員您好：<br>\r\n				感謝您長期的支持與肯定，為了配合財政部推廣發票電子化及加速您的退款作業程序，自2018/10/19起將全面採用電子發票，您可於出貨24小時後至《 訂單查詢 》中查閱訂單發票內容：');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_default_photos`
 --
 
 CREATE TABLE `product_default_photos` (
   `id` int(11) NOT NULL,
-  `inner_photo1` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `inner_photo2` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `inner_photo3` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `inner_photo4` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `inner_photo1` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `inner_photo2` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `inner_photo3` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `inner_photo4` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `product_list_ref` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 資料表的匯出資料 `product_default_photos`
+-- Dumping data for table `product_default_photos`
 --
 
 INSERT INTO `product_default_photos` (`id`, `inner_photo1`, `inner_photo2`, `inner_photo3`, `inner_photo4`, `product_list_ref`) VALUES
@@ -89,21 +119,21 @@ INSERT INTO `product_default_photos` (`id`, `inner_photo1`, `inner_photo2`, `inn
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `product_item_detail`
+-- Table structure for table `product_item_detail`
 --
 
 CREATE TABLE `product_item_detail` (
   `id` int(11) NOT NULL,
   `title` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `item_id` int(11) NOT NULL,
-  `color` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `color` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `size` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `main_photo_substitute` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `main_photo_substitute` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `product_list_ref` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 資料表的匯出資料 `product_item_detail`
+-- Dumping data for table `product_item_detail`
 --
 
 INSERT INTO `product_item_detail` (`id`, `title`, `item_id`, `color`, `size`, `main_photo_substitute`, `product_list_ref`) VALUES
@@ -115,97 +145,104 @@ INSERT INTO `product_item_detail` (`id`, `title`, `item_id`, `color`, `size`, `m
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `product_list`
+-- Table structure for table `product_list`
 --
 
 CREATE TABLE `product_list` (
   `id` int(11) NOT NULL,
   `active_stat` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `activity_text` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `img_url_itemonly` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `img_url_withmodel` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `img_url1` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `img_url2` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `title` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `price_org` int(11) NOT NULL,
-  `price_dis` int(11) NOT NULL,
+  `price_org` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `price_dis` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `category_main` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
-  `category_sub` varchar(45) COLLATE utf8_unicode_ci NOT NULL
+  `category_sub` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `discount` varchar(45) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 資料表的匯出資料 `product_list`
+-- Dumping data for table `product_list`
 --
 
-INSERT INTO `product_list` (`id`, `active_stat`, `activity_text`, `img_url_itemonly`, `img_url_withmodel`, `title`, `price_org`, `price_dis`, `category_main`, `category_sub`) VALUES
-(1, 'p_active_media', 'sale', 'images/men/accessories/a001a.jpg', 'images/men/accessories/a001a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'men', 'upper'),
-(2, 'p_active_media bg_g', 'new', 'images/men/coat/ct001a.jpg', 'images/men/coat/ct001a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'men', 'upper'),
-(3, '', '', 'images/men/home$inside/h002a.jpg', 'images/men/home$inside/h002a-2.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'women', 'coat'),
-(4, '', '', 'images/women/sweater/sw001a.jpg', 'images/women/sweater/sw001a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'kid', 'upper'),
-(5, '', '', 'images/women/shirt/s004a.jpg', 'images/women/shirt/s004a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'men', 'shoes'),
-(6, '', '', 'images/women/sweater/sw002a.jpg', 'images/women/sweater/sw002a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 261, 'women', 'upper'),
-(7, '', '', 'images/women/coat/ct003a.jpg', 'images/women/coat/ct003a-3.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'men', 'upper'),
-(8, '', '', 'images/women/sweater/sw004a.jpg', 'images/women/sweater/sw004a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'kid', 'upper'),
-(9, '', '', 'images/men/accessories/a001a.jpg\r\n', 'images/men/accessories/a001a-1.jpg', '棉質條紋寬領長袖T恤-女', 290, 260, 'men', 'upper');
+INSERT INTO `product_list` (`id`, `active_stat`, `activity_text`, `img_url1`, `img_url2`, `title`, `price_org`, `price_dis`, `category_main`, `category_sub`, `discount`) VALUES
+(1, '', '', 'images/men/accessories/a001a-2.jpg', 'images/men/accessories/a001a-1.jpg', '棉質條紋寬領長袖T恤-男', '$379.00', '', 'men', '配件', 'product-price'),
+(2, '', '', 'images/women/accessories/a001a-2.jpg', 'images/women/accessories/a001a-1.jpg', '棉質條紋寬領長袖T恤-女', '$299.00', '', 'women', '配件', 'product-price'),
+(3, '', 'SALE', 'images/women/accessories/a002a-2.jpg', 'images/women/accessories/a002a-1.jpg', '棉質條紋寬領長袖T恤-女', '$499.00', '$399.00', 'women', '配件', 'product-price-discount'),
+(4, '', '', 'images/men/coat/ct001a-2.jpg', 'images/men/coat/ct001a-1.jpg', '棉質條紋寬領長袖T恤-男', '$999.00', '', 'kid', '外套', 'product-price'),
+(5, '', '', 'images/women/home&inside/h002a-2.jpg', 'images/women/home&inside/h002a-1.jpg', '棉質條紋寬領長袖T恤-女', '$999.00', '', 'men', '居家', 'product-price'),
+(6, '', '', 'images/women/shirt/s004a-2.jpg', 'images/women/shirt/s004a-1.jpg', '棉質條紋寬領長袖T恤-男', '$999.00', '', 'men', '外套', 'product-price'),
+(7, '', '', 'images/women/coat/ct003a.jpg', 'images/women/coat/ct003a-3.jpg', '棉質條紋寬領長袖T恤-女', '290', '260', 'men', 'upper', ''),
+(8, '', '', 'images/women/sweater/sw004a.jpg', 'images/women/sweater/sw004a-1.jpg', '棉質條紋寬領長袖T恤-女', '290', '260', 'kid', 'upper', ''),
+(9, '', '', 'images/men/accessories/a001a.jpg\r\n', 'images/men/accessories/a001a-1.jpg', '棉質條紋寬領長袖T恤-女', '290', '260', 'men', 'upper', '');
 
 --
--- 已匯出資料表的索引
+-- Indexes for dumped tables
 --
 
 --
--- 資料表索引 `header_ad`
+-- Indexes for table `header_ad`
 --
 ALTER TABLE `header_ad`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `member`
+-- Indexes for table `member`
 --
 ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `product_default_photos`
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_default_photos`
 --
 ALTER TABLE `product_default_photos`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `product_item_detail`
+-- Indexes for table `product_item_detail`
 --
 ALTER TABLE `product_item_detail`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `product_list`
+-- Indexes for table `product_list`
 --
 ALTER TABLE `product_list`
   ADD PRIMARY KEY (`id`);
 
 --
--- 在匯出的資料表使用 AUTO_INCREMENT
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- 使用資料表 AUTO_INCREMENT `header_ad`
+-- AUTO_INCREMENT for table `header_ad`
 --
 ALTER TABLE `header_ad`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- 使用資料表 AUTO_INCREMENT `member`
+-- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- 使用資料表 AUTO_INCREMENT `product_default_photos`
+-- AUTO_INCREMENT for table `product_default_photos`
 --
 ALTER TABLE `product_default_photos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- 使用資料表 AUTO_INCREMENT `product_item_detail`
+-- AUTO_INCREMENT for table `product_item_detail`
 --
 ALTER TABLE `product_item_detail`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- 使用資料表 AUTO_INCREMENT `product_list`
+-- AUTO_INCREMENT for table `product_list`
 --
 ALTER TABLE `product_list`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
